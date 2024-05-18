de00c134d607d37d8f2063d36df15741991e1092
jairomoj <58726090+jairomoj@users.noreply.github.com>
2024-05-18 16:34:36 +0000

    Primera corrección

--- src/app.js
+++ src/app.js
@@ -8,4 +8,21 @@ import "./assets/img/4geeks.ico";
 window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
+
+  console.log("----- Generador de Nombres de Dominio -----");
+
+  let pronoun = ["the", "our"];
+  let adj = ["great", "big"];
+  let noun = ["jogger", "racoon"];
+  let ext = [".com", ".net"];
+
+  for (let i = 0; i < pronoun.length; i++) {
+    for (let j = 0; j < adj.length; j++) {
+      for (let k = 0; k < noun.length; k++) {
+        for (let l = 0; l < ext.length; l++) {
+          console.log(pronoun[i] + adj[j] + noun[k] + ext[l]);
+        }
+      }
+    }
+  }
 };
